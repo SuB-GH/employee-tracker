fbce9f0fc9875d4e9d4262418023b4a0995e1b75
Susan Brown <brown684408@gmail.com>
2022-08-16 11:36:44 -0500

    add inquirer questions

--- routes/apiRoutes/index.js
+++
@@ -1,5 +0,0 @@
-router.use(require('./departmentRoutes'));
-router.use(require('./employeeRoutes'));
-router.use(require('./roleRoutes'));
-
-module.exports = router;
--- server.js
+++ server.js
@@ -1,23 +1,43 @@
+const inquirer = require('inquirer');
 require("dotenv").config()
-const dotenv = require("dotenv")
-dotenv.config()
-const PORT = process.env.PORT || 3001;
+// const dotenv = require("dotenv")
+// dotenv.config()
 
 const db = require('./db/connection');
 
-// Use apiRoutes
-app.use('/api', apiRoutes);
 
-// Default response for any other request (Not Found)
-app.use((req, res) => {
-  res.status(404).end();
-});
+//inquirer prompts
+inquirer.prompt([
+  {
+    type: 'list',
+    name: 'navigation',
+    message: 'What would you like to do?',
+    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+  }
+])
+  .then(({ navigation }) => {
+    console.log(navigation);
+
+    var name = "View all departments"
+    switch (name) {
+      case "View all departments":
+        console.log("show table containing dept names and ids");
+        
+
+        // switch (name) {
+        //   case "View all roles":
+        //     console.log("show job title, role id, salary");
+
+        // }
+    }});
+
 
 // Start server after DB connection
-db.connect(err => {
-  if (err) throw err;
-  console.log('Database connected.');
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-});
+// db.connect(err => {
+//   if (err) throw err;
+//   console.log('Database connected.');
+//   app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+//   });
+// });
+
