e8048108d1761b905c79cba5d2a7974e66da9fc8
Susan Brown <brown684408@gmail.com>
2022-08-17 14:59:24 -0500

    update inquirer prompt for add dept

--- server.js
+++ server.js
@@ -28,31 +28,76 @@ inquirer.prompt([
 
       //this works!
       case "View all departments":
-        console.log("I just selected view all departments!");
-        
+        console.log("This will show a formatted table showing department names and department ids");
+
         db.query(`SELECT * FROM department`, (err, rows) => {
           console.table(rows);
         })
         break;
 
-        //this works!
+      //this works!
       case "View all roles":
-        console.log("I just selected view all roles!");
+        console.log("this will show job title, role id, the department that role belongs to, and the salary for that role");
         db.query(`SELECT * FROM roles`, (err, rows) => {
           console.table(rows);
         })
         break;
 
-        //this works!
+      //this works!
       case "View all employees":
-        console.log("I just selected view all employees!");
+        console.log("this will show formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to");
         db.query(`SELECT * FROM employee`, (err, rows) => {
           console.table(rows);
         })
         break;
 
-        case "Add a department":
-        console.log("I want to add a department!");
+      //this works!
+      // case "Add a department":
+      // console.log("this will prompt me to enter the name of the department and that department is added to the database");
+      // db.query(`SELECT * FROM employee`, (err, rows) => {
+      //   console.table(rows);
+      // })
+      // break;
+
+      
+      case "Add a department":
+        //console.log("this will prompt me to enter the name of the department and that department is added to the database");
+        inquirer.prompt(
+          {
+            type: 'input',
+            name: 'newDept',
+            message: 'What is the name of the department?',
+            value: "", 
+          
+          },
+        )
+        
+        db.connect(function(err) {
+          if (err) throw err;
+          var sql = "INSERT INTO department (dept_name, dept_id) VALUES ('Engineering', '02')";
+        db.query(sql, function (err, result) {
+          if (err) throw err;
+          console.log("1 record inserted");
+        })
+      });
+        break;
+
+      case "Add a role":
+        console.log("this will prompt me to enter the name, salary, and department for the role and that role is added to the database");
+        db.query(`SELECT * FROM employee`, (err, rows) => {
+          console.table(rows);
+        })
+        break;
+
+      case "Add an employee":
+        console.log("this will prompt me to enter the employee’s first name, last name, role, and manager, and that employee is added to the database");
+        db.query(`SELECT * FROM employee`, (err, rows) => {
+          console.table(rows);
+        })
+        break;
+
+      case "Update an employee role":
+        console.log("this will prompt me to select an employee to update and their new role and this information is updated in the database");
         db.query(`SELECT * FROM employee`, (err, rows) => {
           console.table(rows);
         })
